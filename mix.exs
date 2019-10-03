39fe2a8bed25cdd0b1321a1c4408cee18e50d7c1
AaronShea <ashea@tinfoilsecurity.com>
2019-10-03 15:49:48 -0700

    add mix info and license

--- mix.exs
+++ mix.exs
@@ -7,21 +7,37 @@ defmodule Wormwood.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:absinthe, "~> 1.4"}
+    ]
+  end
+
+  defp description do
+    """
+    Wormwood is a tiny library to aid in testing GraphQL queries against an Absinthe schema.
+    It allows you to test your query documents inside ExUnit test modules, and requires no HTTP requests to occur during testing.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Aaron Shea", "Tinfoil Security Inc."],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tinfoil/wormwood"}
     ]
   end
 end
