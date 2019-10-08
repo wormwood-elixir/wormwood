764c3bd314b9c198ba870ef4088f58efad8f6a49
AaronShea <ashea@tinfoilsecurity.com>
2019-10-08 15:41:03 -0700

    pre-release version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wormwood.MixProject do
   def project do
     [
       app: :wormwood,
-      version: "1.0.0",
+      version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
