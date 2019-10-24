4f7b9e68393c4bab4dc3623e29d5cf2f6e3683bc
Shane Wilton <shane.wilton@gmail.com>
2019-10-23 21:03:49 -0700

    Move test modules from lib/examples to test/support
    This avoids needing to bundle the test code with production builds

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Wormwood.MixProject do
       app: :wormwood,
       version: "0.1.0",
       elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
@@ -24,6 +25,9 @@ defmodule Wormwood.MixProject do
       extra_applications: [:logger]
     ]
   end
+  
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
